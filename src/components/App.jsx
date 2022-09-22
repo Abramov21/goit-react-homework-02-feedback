33ffe585faaaedfe82c645997da88e559641471e
Abramov21 <104825295+Abramov21@users.noreply.github.com>
2022-09-22 16:26:28 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,103 +1,93 @@
-import { React, Component } from 'react';
-import Section from './Section';
-import Statistics from './Statistics/Statistics';
-import Notification from './Notification';
-import FeedbackOptions from './FeedbackOptions';
+import React from 'react';
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import PhoneBookList from './PhoneBookList';
+import Form from './Form';
+import Filter from './Filter';
 
-export default class App extends Component {
+class App extends Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    phoneList: [
+      { id: 'id-1', name: 'Tony Stark', number: '459-12-56' },
+      { id: 'id-2', name: 'Stephen Rogers', number: '443-89-12' },
+      { id: 'id-3', name: 'Bruce Banner', number: '645-17-79' },
+      { id: 'id-4', name: 'Thor Odinson', number: '227-84-62' },
+      { id: 'id-5', name: 'Natasha Romanoff', number: '207-91-27'},
+      { id: 'id-6', name: 'Clint Barton', number: '564-92-48' },
+    ],
+    filter: '',
   };
 
+  formSubmitHendler = data => {
+    // console.log(data);
+    const { phoneList } = this.state;
+    const PhonebookListItem = {
+      id: nanoid(),
+      name: data.name,
+      number: data.number,
+    };
+    if (phoneList.find(item => item.name === PhonebookListItem.name)) {
+      return alert(`${PhonebookListItem.name}  is olrady in contacts`);
+    }
+    this.setState(prevState => ({
+      phoneList: [PhonebookListItem, ...prevState.phoneList],
+    }));
+  };
 
-  countTotalFeedback(){
-    const { good, bad, neutral} = this.state;
-    return good+bad+neutral
-  }
+  deletePhoneListItem = phoneListId => {
+    this.setState(prevState => ({
+      phoneList: prevState.phoneList.filter(
+        phoneListItem => phoneListItem.id !== phoneListId
+      ),
+    }));
+  };
 
-  countPositiveFeedbackPercentage(){
-    const { good, bad, neutral} = this.state;
-    const PositivePercentage = Math.round(
-      (good / (good + bad + neutral)) * 100
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  getVisibleFilter = () => {
+    const { phoneList, filter } = this.state;
+
+    const normalizedFilter = filter.toLowerCase();
+
+    return phoneList.filter(item =>
+      item.name.toLowerCase().includes(normalizedFilter)
     );
-    return PositivePercentage;
+  };
+
+  componentDidMount(){
+    const localPhonelist = localStorage.getItem('phoneList');
+    const parsePhonelist = JSON.parse(localPhonelist);
+    if(parsePhonelist){
+      this.setState({phoneList: parsePhonelist})
+    }
   }
 
-  handleIncrement = (option) => {
-    this.setState((prevState) => {
-      return {
-        [option]: prevState[option] +1,
-      }
-     })
+  componentDidUpdate(prevProps, prevState){
+    // console.log(prevState);
+    if (this.state.phoneList !== prevState.phoneList
+      ) {
+        localStorage.setItem('phoneList', JSON.stringify(this.state.phoneList))
+    }
   }
-
 
   render() {
-    const { good,bad,neutral} =this.state;
-    const options = Object.keys(this.state)
-    const total = this.countTotalFeedback()
-    const positivePercentage = this.countPositiveFeedbackPercentage()
+    const { filter } = this.state;
+
+    const filteredPhonebookList = this.getVisibleFilter();
 
     return (
-      <Section title="Please leave feedback">
-        <FeedbackOptions options={options} setFeedback={this.handleIncrement} />
+      <>
+        <Form onSubmit={this.formSubmitHendler} />
 
-        {total > 0 ? (
-            <Statistics
-            good={good}
-            bad={bad}
-            neutral={neutral}
-            total ={total}
-            positivePercentage ={positivePercentage}
-            />
-          ) : (
-            <Notification massege="There is no feedback" />
-          )}
-      </Section>
-      )
+        <Filter value={filter} onChange={this.changeFilter} />
+        <PhoneBookList
+          phoneList={filteredPhonebookList}
+          onDeletePhoneListItem={this.deletePhoneListItem}
+        />
+      </>
+    );
   }
 }
-
-
-
-// state = { isOpen: false };
-
-// toggle = () => {
-//   this.setState(state => ({ isOpen: !state.isOpen }));
-// };
-
-// render() {
-//   const { isOpen } = this.state;
-//   const { children } = this.props;
-
-//   return (
-//     <div>
-//       <button onClick={this.toggle}>{isOpen ? "Hide" : "Show"}</button>
-//       {isOpen && children}
-//     </div>
-//   );
-// }
-// }
-
-// class Toggle extends Component {
-//   state = { isOpen: false };
-
-//   show = () => this.setState({ isOpen: true });
-
-//   hide = () => this.setState({ isOpen: false });
-
-//   render() {
-//     const { isOpen } = this.state;
-//     const { children } = this.props;
-
-//     return (
-//       <>
-//         <button onClick={this.show}>Show</button>
-//         <button onClick={this.hide}>Hide</button>
-//         {isOpen && children}
-//       </>
-//     );
-//   }
-// }
+export default App;
