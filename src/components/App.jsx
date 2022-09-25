b556e8543e14ad6f4ce2d6766e629e3fed9abbbf
Abramov21 <104825295+Abramov21@users.noreply.github.com>
2022-09-25 10:05:55 +0300

    app fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,54 +12,55 @@ class App extends Component {
     bad: 0,
   };
 
-
-  countTotalFeedback(){
-    const { good, bad, neutral} = this.state;
-    return good+bad+neutral
+  countTotalFeedback() {
+    const { good, bad, neutral } = this.state;
+    return good + bad + neutral;
   }
 
-  countPositiveFeedbackPercentage(){
-    const { good, bad, neutral} = this.state;
+  countPositiveFeedbackPercentage() {
+    const { good, bad, neutral } = this.state;
     const PositivePercentage = Math.round(
       (good / (good + bad + neutral)) * 100
     );
     return PositivePercentage;
   }
 
-  handleIncrement = (option) => {
-    this.setState((prevState) => {
+  handleIncrement = option => {
+    this.setState(prevState => {
       return {
-        [option]: prevState[option] +1,
-      }
-     })
-  }
-
+        [option]: prevState[option] + 1,
+      };
+    });
+  };
 
   render() {
-    const { good,bad,neutral} =this.state;
-    const options = Object.keys(this.state)
-    const total = this.countTotalFeedback()
-    const positivePercentage = this.countPositiveFeedbackPercentage()
+    const { good, bad, neutral } = this.state;
+    const options = Object.keys(this.state);
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
 
     return (
-        <div>
-             <Section title="Please leave feedback">
-        <FeedbackOptions options={options} setFeedback={this.handleIncrement} />
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            setFeedback={this.handleIncrement}
+          />
 
-        {total > 0 ? (
+          {total > 0 ? (
             <Statistics
-            good={good}
-            bad={bad}
-            neutral={neutral}
-            total ={total}
-            positivePercentage ={positivePercentage}
+              good={good}
+              bad={bad}
+              neutral={neutral}
+              total={total}
+              positivePercentage={positivePercentage}
             />
           ) : (
             <Notification massege="There is no feedback" />
           )}
-      </Section>
-          </div>
-      )
+        </Section>
+      </div>
+    );
   }
 }
 
